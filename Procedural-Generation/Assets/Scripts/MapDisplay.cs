9f14ddc90cb5276f0ddd6222828974e8b9734117
Faiz Qureshi <faq588@mail.usask.ca>
2019-11-10 11:57:40 -0700

    worte the scrip MapDisplay and mapGenerator to respectivly generate and display the noise map

---
+++ Procedural-Generation/Assets/Scripts/MapDisplay.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// class responsible for taking the noise map and turning it into a texture then applying
+// that texture to a plane
+public class MapDisplay : MonoBehaviour
+{
+    // reference to the texture of the plane
+    public Renderer textureRender;
+
+    public void DrawNoiseMap(float[,] noiseMap)
+    {
+        // getting width and height of noise map
+        int width = noiseMap.GetLength(0);
+        int height = noiseMap.GetLength(1);
+
+        Texture2D texture = new Texture2D(width, height);
+
+        // creating a colour map and assigning it colours
+        Color[] colourMap = new Color[width * height];
+        for(int y = 0; y < height; y++)
+        {
+            for(int x = 0; x < width; x++)
+            {
+                colourMap[y * width + x] = Color.Lerp(Color.black, Color.white, noiseMap[x, y]); 
+            }
+        }
+        texture.SetPixels(colourMap);
+        texture.Apply();
+
+        textureRender.sharedMaterial.mainTexture = texture;
+        textureRender.transform.localScale = new Vector3(width, 1, height); 
+    }
+}
---
+++ Procedural-Generation/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// class responsible for generating noise map by calling Noise class
+public class MapGenerator : MonoBehaviour
+{
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+
+    public void GenerateMap()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        display.DrawNoiseMap(noiseMap); 
+    }
+
+}
