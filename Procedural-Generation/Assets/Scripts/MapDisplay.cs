d93b3bc3f2c72caa271b07ee01b1d5a4eeb839b0
Faiz Qureshi <faq588@mail.usask.ca>
2019-11-10 18:28:50 -0700

    added a mesh Game component so that i can scale up or down the various heights (mountains, water, etc). Added another script called MeshGenerator that generate a mesh, this mesh is then applied to the map using the MapDisplay class as well as the MapGenerator class.

--- Procedural-Generation/Assets/Scripts/MapDisplay.cs
+++ Procedural-Generation/Assets/Scripts/MapDisplay.cs
@@ -8,10 +8,19 @@ public class MapDisplay : MonoBehaviour
 {
     // reference to the texture of the plane
     public Renderer textureRender;
+	// ref to mesh filter and render
+	public MeshFilter meshFilter;
+	public MeshRenderer meshRenderer;
 
     public void DrawTexture(Texture2D texture)
     {
         textureRender.sharedMaterial.mainTexture = texture;
         textureRender.transform.localScale = new Vector3(texture.width, 1, texture.height); 
     }
+
+    public void DrawMesh(MeshData meshData, Texture2D texture)
+	{
+		meshFilter.sharedMesh = meshData.CreateMesh();
+		meshRenderer.sharedMaterial.mainTexture = texture; 
+	}
 }
--- Procedural-Generation/Assets/Scripts/MapGenerator.cs
+++ Procedural-Generation/Assets/Scripts/MapGenerator.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class MapGenerator : MonoBehaviour
 {
     // enum to switch between drawing the colour and noise maps
-    public enum DrawMode { NoiseMap, ColourMap };
+    public enum DrawMode { NoiseMap, ColourMap, Mesh };
 	public DrawMode drawMode;
 
 	public int mapWidth;
@@ -55,6 +55,10 @@ public class MapGenerator : MonoBehaviour
 		{
 			display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
 
+		}
+        else if (drawMode == DrawMode.Mesh)
+		{
+			display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap), TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight)); 
 		}
 	}
 
